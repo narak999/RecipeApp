c8986f39e5550b6b257a8fa30abf42f104d6581d
BUNNARAK THENG <bunnarak.theng@gm.com>
2023-03-16 21:55:46 -0400

    First Commit

--- app/src/main/java/com/bunnarak/recipeapp/MainActivity.kt
+++ app/src/main/java/com/bunnarak/recipeapp/MainActivity.kt
@@ -3,30 +3,31 @@ package com.bunnarak.recipeapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity(){
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        setContent {
-            Column(
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Text(text = "Hey look some text")
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                Button(onClick = {}) {
-                    Text(text = "A BUTTON")
-                }
-            }
-        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, RecipeListFragment())
+            .commit()
     }
 }
